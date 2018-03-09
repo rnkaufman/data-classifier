9a2f3ed152bf6f7fd9764f5e46652a245a7ec681
rnkaufman <nina@procore.com>
2018-03-09 22:25:02 +0000

    Remove unneeded method

--- app/assets/javascripts/training.js
+++ app/assets/javascripts/training.js
@@ -6,11 +6,6 @@ document.onkeydown = function(e) {
   }
 };
 
-function switchLabels() {
-  var data = document.getElementById('training_data').innerHTML;
-  var chosen_labels = JSON.parse(localStorage.getItem('labels'));
-}
-
 function fetchData() {
   var display_data = JSON.parse(localStorage.getItem('datum'));
   var training_data = document.getElementById('display_data');
